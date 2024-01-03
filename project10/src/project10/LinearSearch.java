a03bb71857b7e78995d365e9d5d770993c7a16c9
mounika-golla <144805119+mounika-golla@users.noreply.github.com>
2024-01-03 23:56:22 +0530

    Update LinearSearch.java

--- project10/src/project10/LinearSearch.java
+++ project10/src/project10/LinearSearch.java
@@ -1,26 +1,21 @@
 package project10;
 
 public class LinearSearch {
-
-    // Function to perform linear search
     static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
-                return i; // Return the index if target is found
+                return i; 
             }
         }
-        return -1; // Return -1 if target is not found
+        return -1; 
     }
 
     public static void main(String[] args) {
-        // Example usage
+       
         int[] array = {2, 7, 1, 4, 6, 9, 3, 8};
         int targetValue = 6;
-
-        // Perform linear search
         int result = linearSearch(array, targetValue);
 
-        // Display the result
         if (result != -1) {
             System.out.println("Target value found at index: " + result);
         } else {
